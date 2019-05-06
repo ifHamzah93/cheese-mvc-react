b7a749c783eea3c1edcb57f9167ba031e14d4931
Vamp <25523682+the-vampiire@users.noreply.github.com>
2019-05-06 00:02:43 -0400

    implemented routes

--- code/src/Routes.js
+++ code/src/Routes.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
+import HomeView from "./views/HomeView";
+import MenusView from "./views/MenusView";
+import CheesesView from "./views/CheesesView";
+import CategoriesView from "./views/CategoriesView";
+
 const Routes = () => (
-  <Switch>
-    {/* we will implement these Route components later */}
-    <Route exact path="" component={} />
-  </Switch>
+	<Switch>
+		<Route exact path="/" component={HomeView} />
+		<Route exact path="/cheeses" component={CheesesView} />
+		<Route exact path="/categories" component={CategoriesView} />
+		<Route exact path="/menus" component={MenusView} />
+	</Switch>
 );
 
 export default Routes;
