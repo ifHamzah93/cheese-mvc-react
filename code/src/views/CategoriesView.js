1b22e052cfbe0022c597acfb1f7d85adeea3bbff
Vamp <25523682+the-vampiire@users.noreply.github.com>
2019-05-06 00:04:38 -0400

    CategoriesView complete

---
+++ code/src/views/CategoriesView.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import request from "../utilities/api-request";
+import CategoryForm from "../components/category/CategoryForm";
+import CategoriesList from "../components/category/CategoriesList";
+
+class CategoriesView extends Component {
+	state = {
+		categories: [],
+	};
+
+	async componentDidMount() {
+		const res = await request.get("/categories");
+		const categories = res.data;
+
+		this.setState({ categories });
+	}
+
+	addToCategories = category =>
+		this.setState(state => {
+			const { categories } = state;
+
+			return { categories: [category, ...categories] };
+		});
+
+	render() {
+		const { categories } = this.state;
+
+		return (
+			<Container>
+				<Row>
+					<CategoryForm addCategory={this.addToCategories} />
+				</Row>
+				<br />
+				<Row>
+					<CategoriesList categories={categories} />
+				</Row>
+			</Container>
+		);
+	}
+}
+
+export default CategoriesView;
