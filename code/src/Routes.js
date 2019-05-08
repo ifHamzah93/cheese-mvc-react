f41e5266ce5104cbe50ab78599ff741336995d49
Vamp <25523682+the-vampiire@users.noreply.github.com>
2019-05-08 01:40:22 -0400

    MenuView route

--- code/src/Routes.js
+++ code/src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import HomeView from "./views/HomeView";
+import MenuView from "./views/MenuView";
 import MenusView from "./views/MenusView";
 import CheesesView from "./views/CheesesView";
 import CategoriesView from "./views/CategoriesView";
@@ -9,9 +10,10 @@ import CategoriesView from "./views/CategoriesView";
 const Routes = () => (
 	<Switch>
 		{/* <Route exact path="/" component={HomeView} /> */}
+		<Route exact path="/menus" component={MenusView} />
 		<Route exact path="/cheeses" component={CheesesView} />
 		<Route exact path="/categories" component={CategoriesView} />
-		{/* <Route exact path="/menus" component={MenusView} /> */}
+		<Route exact path="/menus/:menuID" component={MenuView} />
 	</Switch>
 );
 
