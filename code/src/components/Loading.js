3c80dfe6b09b581f24dbd4224761d566466b354a
Vamp <25523682+the-vampiire@users.noreply.github.com>
2019-05-08 02:02:37 -0400

    Loading fixed positioning

--- code/src/components/Loading.js
+++ code/src/components/Loading.js
@@ -1,16 +1,17 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
 import Spinner from "react-bootstrap/Spinner";
 
 const Loading = () => (
-  <div style={{ margin: "0 auto" }}>
-    <Spinner animation="grow" variant="primary" />
-    <Spinner animation="grow" variant="secondary" />
-    <Spinner animation="grow" variant="success" />
-    <Spinner animation="grow" variant="danger" />
-    <Spinner animation="grow" variant="warning" />
-    <Spinner animation="grow" variant="info" />
-    <Spinner animation="grow" variant="dark" />
-  </div>
+		<div style={{ marginLeft: "50px" }}>
+			<Spinner animation="grow" variant="primary" />
+			<Spinner animation="grow" variant="secondary" />
+			<Spinner animation="grow" variant="success" />
+			<Spinner animation="grow" variant="danger" />
+			<Spinner animation="grow" variant="warning" />
+			<Spinner animation="grow" variant="info" />
+			<Spinner animation="grow" variant="dark" />
+		</div>
 );
 
 export default Loading;
